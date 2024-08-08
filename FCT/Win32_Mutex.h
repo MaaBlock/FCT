4f47b621746b9ba8a46d6a34389e1abee0ebb3fa
MaBlock <2985861993@qq.com>
2024-08-08 17:21:44 +0800

    增加window平台下的mutex接口实现

--- FCT/Win32_Mutex.h
+++ FCT/Win32_Mutex.h
@@ -1,6 +1,23 @@
 class Win32_Mutex : public Mutex {
 public:
+	Win32_Mutex() {
 
+	}
+	~Win32_Mutex() {
+		destory();
+	}
+	void create() {
+		if (m_mutex) {
+			destory();
+		}
+		m_mutex = CreateMutexA(NULL, FALSE, NULL);
+	}
+	void destory() {
+		if (m_mutex) {
+			CloseHandle(m_mutex);
+			m_mutex = NULL;
+		}
+	}
 private:
-
+	HANDLE m_mutex;
 };
