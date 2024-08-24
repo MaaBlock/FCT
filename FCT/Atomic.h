80bc405e9daecbf1ceea03ff14b003b42a8f2a5b
MaBlock <2985861993@qq.com>
2024-08-24 19:17:38 +0800

    Atomic修改为不使用c++11的特性

--- FCT/Atomic.h
+++ FCT/Atomic.h
@@ -1,60 +1,58 @@
-#pragma once
 template <typename T>
 class Atomic {
 public:
     Atomic(T value = 0) : m_value(value) {}
     T operator++() {
-        if constexpr (std::is_pointer_v<T>) {
 #ifdef _WIN64
+        if (sizeof(T) == sizeof(void*)) {
             return InterlockedIncrement64(reinterpret_cast<volatile LONGLONG*>(&m_value));
-#else
-            return InterlockedIncrement(reinterpret_cast<volatile LONG*>(&value_));
-#endif
         }
         else {
             return InterlockedIncrement(reinterpret_cast<volatile LONG*>(&m_value));
         }
+#else
+        return InterlockedIncrement(reinterpret_cast<volatile LONG*>(&m_value));
+#endif
     }
     T operator--() {
-        if constexpr (std::is_pointer_v<T>) {
 #ifdef _WIN64
+        if (sizeof(T) == sizeof(void*)) {
             return InterlockedDecrement64(reinterpret_cast<volatile LONGLONG*>(&m_value));
-#else
-            return InterlockedDecrement(reinterpret_cast<volatile LONG*>(&value_));
-#endif
         }
         else {
             return InterlockedDecrement(reinterpret_cast<volatile LONG*>(&m_value));
         }
+#else
+        return InterlockedDecrement(reinterpret_cast<volatile LONG*>(&m_value));
+#endif
     }
     T operator=(T newValue) {
-        if constexpr (std::is_pointer_v<T>) {
 #ifdef _WIN64
+        if (sizeof(T) == sizeof(void*)) {
             return InterlockedExchange64(reinterpret_cast<volatile LONGLONG*>(&m_value), reinterpret_cast<LONGLONG>(newValue));
-#else
-            return InterlockedExchange(reinterpret_cast<volatile LONG*>(&value_), reinterpret_cast<LONG>(newValue));
-#endif
         }
         else {
-            return InterlockedExchange(reinterpret_cast<volatile LONG*>(&m_value), static_cast<LONG>(newValue));
+            return InterlockedExchange(reinterpret_cast<volatile LONG*>(&m_value), reinterpret_cast<LONG>(newValue));
         }
+#else
+        return InterlockedExchange(reinterpret_cast<volatile LONG*>(&m_value), static_cast<LONG>(newValue));
+#endif
     }
     T ifEqualThenAssign(T newValue, T comparand) {
-        if constexpr (std::is_pointer_v<T>) {
 #ifdef _WIN64
+        if (sizeof(T) == sizeof(void*)) {
             return InterlockedCompareExchange64(reinterpret_cast<volatile LONGLONG*>(&m_value), reinterpret_cast<LONGLONG>(newValue), reinterpret_cast<LONGLONG>(comparand));
-#else
-            return InterlockedCompareExchange(reinterpret_cast<volatile LONG*>(&value_), reinterpret_cast<LONG>(newValue), reinterpret_cast<LONG>(comparand));
-#endif
         }
         else {
-            return InterlockedCompareExchange(reinterpret_cast<volatile LONG*>(&m_value), static_cast<LONG>(newValue), static_cast<LONG>(comparand));
+            return InterlockedCompareExchange(reinterpret_cast<volatile LONG*>(&m_value), reinterpret_cast<LONG>(newValue), reinterpret_cast<LONG>(comparand));
         }
+#else
+        return InterlockedCompareExchange(reinterpret_cast<volatile LONG*>(&m_value), static_cast<LONG>(newValue), static_cast<LONG>(comparand));
+#endif
     }
     T value() {
         return m_value;
     }
-
 private:
     volatile T m_value;
 };
