fa4f66936b3548d1d82d873e195efab166f31e10
刘敏清 ch <2985861993@qq.com>
2025-08-26 23:58:11 +0800

    统一设置utf8输入编码与utf8执行编码

---
+++ config/set_utf8_compiler_encoding.cmake
@@ -0,0 +1,13 @@
+﻿function(set_utf8_for_target target_name)
+    if(MSVC) # msvc
+        target_compile_options(${target_name} PRIVATE
+                /source-charset:utf-8    # 输入编码
+                /execution-charset:utf-8 # 执行编码
+        )
+    else() # gcc 或 clang
+        target_compile_options(${target_name} PRIVATE
+                -finput-charset=UTF-8     # 输入编码
+                -fexec-charset=UTF-8      # 执行编码
+        )
+    endif()
+endfunction()
