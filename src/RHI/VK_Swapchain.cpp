de44713a3ef5f35ed969d4693694de394ffb758f
MaBlock CN <2985861993@qq.com>
2025-11-13 13:21:06 +0800

    修复gamma矫正(含imgui)

--- src/RHI/VK_Swapchain.cpp
+++ src/RHI/VK_Swapchain.cpp
@@ -186,7 +186,7 @@ namespace FCT {
         vk::SurfaceFormatKHR VK_Swapchain::chooseSwapSurfaceFormat(
                 const std::vector<vk::SurfaceFormatKHR> &availableFormats) {
             for (const auto &availableFormat : availableFormats) {
-                if (availableFormat.format == vk::Format::eB8G8R8A8Unorm &&
+                if (availableFormat.format == vk::Format::eB8G8R8A8Srgb &&
                     availableFormat.colorSpace == vk::ColorSpaceKHR::eSrgbNonlinear) {
                     return availableFormat;
                     }
